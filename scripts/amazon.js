6529e7f11d11b16048e90b3fe3598da91891b580
DeontayPeterson <rnmurphy47@gmail.com>
2023-11-02 16:02:07 -0400

    Make cart quantity interactive.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,6 +79,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         quantity: 1
       })
     }
-    console.log(cart)
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity
+    })
+    document.querySelector('.js-cart-quantity').innerText = cartQuantity;
+
   })
 })
